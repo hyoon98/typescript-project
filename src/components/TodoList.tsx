86a72776e752b83738af2e591b15dd55af68ed58
hyoon1998 <hya62@sfu.ca>
2023-11-12 23:31:08 -0800

    simple todo list

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,19 @@
+import { Todo } from "../model"
+import SingleTodo from "./SingleTodo"
+
+type Props={
+    todos: Todo[],
+    setTodos: React.Dispatch<React.SetStateAction<Todo[]>>
+}
+
+const TodoList = ({todos,setTodos}:Props) => {
+  return (
+    <div className="todos">
+        {todos.map(todo=>(
+            <SingleTodo todo={todo} key={todo.id} todos={todos} setTodos={setTodos}/>
+        ))}
+    </div>
+  )
+}
+
+export default TodoList
